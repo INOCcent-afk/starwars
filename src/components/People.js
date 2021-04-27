f3d9290047b57231e4604c86ce0deb2ad55dc284
Dave Inoc <michael828inoc@gmail.com>
2021-04-27 22:50:31 +0800

    added people

--- src/components/People.js
+++ src/components/People.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { useQuery } from "react-query";
+import Person from "./Person";
+
+const fetchPeople = async () => {
+  const res = await fetch("http://swapi.dev/api/people/");
+  return res.json();
+};
 
 const People = () => {
+  const { data, status } = useQuery("people", fetchPeople);
+
   return (
     <div>
-      <h2>PEOPLE</h2>
+      <h2>People</h2>
+      {status === "loading" && <div>loading data</div>}
+      {status === "success" && (
+        <div>
+          {data.results.map((person) => (
+            <Person planet={person} key={planet.name} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
