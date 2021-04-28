eef5756c94a1d828a1ac3bd8ebb35de81838b99c
Dave Inoc <michael828inoc@gmail.com>
2021-04-28 11:12:13 +0800

    pagination

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Planets from "./components/Planets";
 import People from "./components/People";
+import { ReactQueryDevtools } from "react-query-devtools";
 
 function App() {
   const [page, setPage] = useState("planets");
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import Planet from "./Planet";
 
-const fetchPlanets = async () => {
-  const res = await fetch("http://swapi.dev/api/planets/");
+const fetchPlanets = async (page) => {
+  const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
   return res.json();
 };
 
 const Planets = () => {
-  const { data, status } = useQuery("planets", fetchPlanets);
+  const [page, setPage] = useState(1);
+  const { data, status } = useQuery(["planets", page], () =>
+    fetchPlanets(page)
+  );
 
   return (
     <div>
       <h2>PLANET</h2>
+
       {status === "loading" && <div>loading data</div>}
       {status === "success" && (
         <div>
+          <button onClick={() => setPage((old) => Math.max(old - 1, 1))}>
+            Previous Page
+          </button>
+          <span>{page}</span>
+          <button
+            onClick={() =>
+              setPage((old) => (!data || !data.next ? old : old + 1))
+            }
+          >
+            Next Page
+          </button>
           {data.results.map((planet) => (
             <Planet planet={planet} key={planet.name} />
           ))}
